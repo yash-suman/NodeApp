26c2c798bafeb7f0ccebdfdc48a57a097e27bb66
yash-suman <88178234+yash-suman@users.noreply.github.com>
2021-08-11 09:48:51 +0530

    Update main.js

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ var http = require('http');
 // configure our HTTP server
 var server = http.createServer(function (request, response) {
   response.writeHead(200, {"Content-Type": "text/plain"});
-  response.end("Hello secindapp!! \n");
+  response.end("Hello sample!! \n");
 });
 
 // listen on localhost:8000
